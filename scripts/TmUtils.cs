e76dc7a0e474e3e14b20c3dcf0deba3c1139a970
unknown <misawa@elix7i5.(none)>
2013-08-05 09:48:55 +0900

    Polyリングメッシュ作成ユーティリティ追加

--- scripts/TmUtils.cs
+++ scripts/TmUtils.cs
@@ -45,8 +45,8 @@ public class TmUtils {
 			vertices[cnt*2+1] = new Vector3(((float)ix/(float)_width - 0.5f), 0.5f,0.0f);
 			triangles[cnt*2+0] = cnt*2+0;
 			triangles[cnt*2+1] = cnt*2+1;
-			uv[cnt*2+0] = new Vector3(((float)ix/(float)_width),0.0f,0.0f);
-			uv[cnt*2+1] = new Vector3(((float)ix/(float)_width),1.0f,0.0f);
+			uv[cnt*2+0] = new Vector2(vertices[cnt*2+0].x+0.5f,vertices[cnt*2+0].y+0.5f);
+			uv[cnt*2+1] = new Vector2(vertices[cnt*2+1].x+0.5f,vertices[cnt*2+1].y+0.5f);
 			colors[cnt*2+0] = colors[cnt*2+1] = new Color(0.5f,0.5f,0.5f,1.0f);
 			cnt++;
 		}
@@ -55,8 +55,8 @@ public class TmUtils {
 			vertices[cnt*2+1] = new Vector3( 0.5f,((float)iy/(float)_height - 0.5f),0.0f);
 			triangles[cnt*2+0] = cnt*2+0;
 			triangles[cnt*2+1] = cnt*2+1;
-			uv[cnt*2+0] = new Vector3(0.0f,((float)iy/(float)_height),0.0f);
-			uv[cnt*2+1] = new Vector3(1.0f,((float)iy/(float)_height),0.0f);
+			uv[cnt*2+0] = new Vector2(vertices[cnt*2+0].x+0.5f,vertices[cnt*2+0].y+0.5f);
+			uv[cnt*2+1] = new Vector2(vertices[cnt*2+1].x+0.5f,vertices[cnt*2+1].y+0.5f);
 			colors[cnt*2+0] = colors[cnt*2+1] = new Color(0.5f,0.5f,0.5f,1.0f);
 			cnt++;
 		}
@@ -70,6 +70,75 @@ public class TmUtils {
 		mesh.RecalculateBounds ();
 		mesh.Optimize();
 		mesh.SetIndices(mesh.GetIndices(0),MeshTopology.Lines,0);
+		return mesh;
+	}
+	
+	public static Mesh CreateLineCircle(int _vertNum){
+		Vector3[] vertices = new Vector3[(_vertNum+1)];
+		int[] triangles = new int[(((_vertNum+1))/3+1)*3];
+		Vector2[] uv = new Vector2[(_vertNum+1)];
+		Color[] colors = new Color[(_vertNum+1)];
+		Vector3[] normals = new Vector3[(_vertNum+1)];
+
+		int cnt = 0;
+		for(int ii = 0; ii <= _vertNum; ++ii){
+			float fx = Mathf.Cos(Mathf.PI*2.0f * ((float)ii / (float)_vertNum))*0.5f;
+			float fy = Mathf.Sin(Mathf.PI*2.0f * ((float)ii / (float)_vertNum))*0.5f;
+			vertices[cnt] = new Vector3(fx,fy,0.0f);
+			triangles[cnt] = cnt;
+			uv[cnt] = new Vector2(vertices[cnt].x+0.5f,vertices[cnt].y+0.5f);
+			colors[cnt] = new Color(0.5f,0.5f,0.5f,1.0f);
+			cnt++;
+		}
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.uv = uv;
+		mesh.colors = colors;
+		mesh.normals = normals;
+		mesh.RecalculateNormals ();
+		mesh.RecalculateBounds ();
+		mesh.Optimize();
+		mesh.SetIndices(mesh.GetIndices(0),MeshTopology.LineStrip,0);
+		return mesh;
+	}
+	
+	public static Mesh CreatePolyRing(int _vertNum, float _minRad, float _maxRad){
+		Vector3[] vertices = new Vector3[(_vertNum+1)*2];
+		int[] triangles = new int[(_vertNum+1)*6];
+		Vector2[] uv = new Vector2[(_vertNum+1)*2];
+		Color[] colors = new Color[(_vertNum+1)*2];
+		Vector3[] normals = new Vector3[(_vertNum+1)*2];
+
+		int cnt = 0;
+		for(int ii = 0; ii <= _vertNum; ++ii){
+			float fx = Mathf.Cos(Mathf.PI*2.0f * ((float)ii / (float)_vertNum));
+			float fy = Mathf.Sin(Mathf.PI*2.0f * ((float)ii / (float)_vertNum));
+			vertices[cnt*2+0] = new Vector3(fx*_maxRad*0.5f,fy*_maxRad*0.5f,0.0f);
+			vertices[cnt*2+1] = new Vector3(fx*_minRad*0.5f,fy*_minRad*0.5f,0.0f);
+			triangles[cnt*6+0] = cnt*2+0;
+			triangles[cnt*6+1] = cnt*2+1;
+			triangles[cnt*6+2] = (ii<(_vertNum)) ? (cnt*2+2) : 0;
+			triangles[cnt*6+3] = cnt*2+0;
+			triangles[cnt*6+4] = (ii>0) ? (cnt*2-1) : 0;
+			triangles[cnt*6+5] = cnt*2+1;
+//			uv[cnt*2+0] = new Vector2(vertices[cnt*2+0].x+0.5f,vertices[cnt*2+0].y+0.5f);
+//			uv[cnt*2+1] = new Vector2(vertices[cnt*2+1].x+0.5f,vertices[cnt*2+1].y+0.5f);
+			uv[cnt*2+0] = new Vector2((float)ii/(float)_vertNum,0.0f);
+			uv[cnt*2+1] = new Vector2((float)ii/(float)_vertNum,1.0f);
+			colors[cnt*2+0] = colors[cnt*2+1] = new Color(0.5f,0.5f,0.5f,1.0f);
+			cnt++;
+		}
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.uv = uv;
+		mesh.colors = colors;
+		mesh.normals = normals;
+		mesh.RecalculateNormals ();
+		mesh.RecalculateBounds ();
+		mesh.Optimize();
+		mesh.SetIndices(mesh.GetIndices(0),MeshTopology.Triangles,0);
 		return mesh;
 	}
 	
