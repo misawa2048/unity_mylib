43450cd666303675ac7c6c4306ff6cde1423c56d
com-user01 <tsuyoshi-misawa@comcept.jp>
2014-12-30 14:21:06 +0900

    TmMesh : add calcTangents

--- scripts/TmMesh.cs
+++ scripts/TmMesh.cs
@@ -205,7 +205,7 @@ public class TmMesh{
 		mesh.SetIndices(mesh.GetIndices(0),MeshTopology.Triangles,0);
 		return mesh;
 	}
-
+	
 	public static Mesh CreatePoly(int _vertNum, float _ofsDeg=0.0f){
 		return CreatePoly(_vertNum, new Color(0.5f,0.5f,0.5f,1.0f),_ofsDeg,0.0f);
 	}
@@ -348,14 +348,14 @@ public class TmMesh{
 		mesh.Optimize();
 		return mesh;
 	}
-
+	
 	public static Mesh CreateSphere(int _divH, int _divV){
 		return CreateSphere(_divH, _divV, new Color(0.5f,0.5f,0.5f,1.0f));
 	}
-	public static Mesh CreateSphere(int _divH, int _divV, Color _col){
+	public static Mesh CreateSphere(int _divH, int _divV, Color _color){
 		Mesh mesh = new Mesh();
-
-		float radius = 1f;
+		
+		float radius = 0.5f;
 		// Longitude |||
 		int nbLong = _divH;
 		// Latitude ---
@@ -363,6 +363,8 @@ public class TmMesh{
 		
 		#region Vertices
 		Vector3[] vertices = new Vector3[(nbLong+1) * nbLat + 2];
+		int _vertNum = vertices.Length;
+		Color[] cols = new Color[_vertNum];
 		float _pi = Mathf.PI;
 		float _2pi = _pi * 2f;
 		
@@ -376,8 +378,8 @@ public class TmMesh{
 			for( int lon = 0; lon <= nbLong; lon++ )
 			{
 				float a2 = _2pi * (float)(lon == nbLong ? 0 : lon) / nbLong;
-				float sin2 = Mathf.Sin(a2);
-				float cos2 = Mathf.Cos(a2);
+				float sin2 = Mathf.Sin(a2+_pi);
+				float cos2 = Mathf.Cos(a2+_pi);
 				
 				vertices[ lon + lat * (nbLong + 1) + 1] = new Vector3( sin1 * cos2, cos1, sin1 * sin2 ) * radius;
 			}
@@ -387,8 +389,10 @@ public class TmMesh{
 		
 		#region Normales		
 		Vector3[] normales = new Vector3[vertices.Length];
-		for( int n = 0; n < vertices.Length; n++ )
+		for( int n = 0; n < vertices.Length; n++ ){
 			normales[n] = vertices[n].normalized;
+			cols[n] = _color;
+		}
 		#endregion
 		
 		#region UVs
@@ -446,13 +450,77 @@ public class TmMesh{
 		mesh.normals = normales;
 		mesh.uv = uvs;
 		mesh.triangles = triangles;
+		mesh.tangents = CalcTangents(mesh);;
 		
+		mesh.RecalculateNormals ();
 		mesh.RecalculateBounds();
 		mesh.Optimize();
-
+		
 		return mesh;
 	}
-
+	
+	// http://answers.unity3d.com/questions/7789/calculating-tangents-vector4.html
+	public static Vector4[] CalcTangents(Mesh _mesh){
+		long vertexCount = _mesh.vertexCount;
+		long triangleCount = _mesh.triangles.Length;
+		Vector3[] tan1 = new Vector3[vertexCount];
+		Vector3[] tan2 = new Vector3[vertexCount];
+		Vector4[] tangents = new Vector4[vertexCount];
+		for(long a = 0; a< triangleCount; a+=3)
+		{
+			long i1 = _mesh.triangles[a+0];
+			long i2 = _mesh.triangles[a+1];
+			long i3 = _mesh.triangles[a+2];
+			
+			Vector3 v1 = _mesh.vertices[i1];
+			Vector3 v2 = _mesh.vertices[i2];
+			Vector3 v3 = _mesh.vertices[i3];
+			
+			Vector2 w1 = _mesh.uv[i1];
+			Vector2 w2 = _mesh.uv[i2];
+			Vector2 w3 = _mesh.uv[i3];
+			
+			float x1 = v2.x - v1.x;
+			float x2 = v3.x - v1.x;
+			float y1 = v2.y - v1.y;
+			float y2 = v3.y - v1.y;
+			float z1 = v2.z - v1.z;
+			float z2 = v3.z - v1.z;
+			
+			float s1 = w2.x - w1.x;
+			float s2 = w3.x - w1.x;
+			float t1 = w2.y - w1.y;
+			float t2 = w3.y - w1.y;
+			
+			float r = 1.0f / (s1 * t2 - s2 * t1);
+			
+			Vector3 sdir = new Vector3((t2 * x1 - t1 * x2) * r, (t2 * y1 - t1 * y2) * r, (t2 * z1 - t1 * z2) * r);
+			Vector3 tdir = new Vector3((s1 * x2 - s2 * x1) * r, (s1 * y2 - s2 * y1) * r, (s1 * z2 - s2 * z1) * r);
+			
+			tan1[i1] += sdir;
+			tan1[i2] += sdir;
+			tan1[i3] += sdir;
+			
+			tan2[i1] += tdir;
+			tan2[i2] += tdir;
+			tan2[i3] += tdir;
+		}
+		
+		for (long a = 0; a < vertexCount; ++a)
+		{
+			Vector3 n = _mesh.normals[a];
+			Vector3 t = tan1[a];
+			
+			Vector3.OrthoNormalize(ref n, ref t);
+			tangents[a].x = t.x;
+			tangents[a].y = t.y;
+			tangents[a].z = t.z;
+			
+			tangents[a].w = (Vector3.Dot(Vector3.Cross(n, t), tan2[a]) < 0.0f) ? -1.0f : 1.0f;
+		}
+		return tangents;
+	}
+	
 	public static Mesh SetMeshColor(Mesh _nowMesh, Color _col){
 		if(_nowMesh!=null){
 			Color[] cols = new Color[_nowMesh.vertexCount];
@@ -463,5 +531,4 @@ public class TmMesh{
 		}
 		return _nowMesh;
 	}
-	
 }
