f09a9f3d7d5073980adfb0ef8ae1e0a5fc5e79aa
misawa <misawa@elix.jp>
2015-02-01 01:39:10 +0900

    Add TmMaze

--- scripts/TmGesture.cs
+++ scripts/TmGesture.cs
@@ -9,14 +9,15 @@ public class TmGesture{
 			NONE,
 			PINCHI_IN,
 			PINCHI_OUT,
+			LONG_TAP,
+			DOUBLE_TAP,
 			SWYPE_UP,
 			SWYPE_DOWN,
 			SWYPE_LEFT,
 			SWYPE_RIGHT,
-			DOUBLE_TAP,
 		}
 		public Gesture gesture;
-		public int[] fingerId;
+		public Touch[] touches;
 	}
 	
 	private int mMaxTouches;
@@ -24,23 +25,27 @@ public class TmGesture{
 	private float mDblTapTime;
 	private Vector2[] mSttPosition;
 	private Vector2[] mPosition;
+	private float[] mHoldTime;
 	private bool[] mActive;
 	private TmTouchEvent.Gesture[] mGesture;
 	
 	public delegate void OnGestureHandler(TmTouchEvent eve);
 	public event OnGestureHandler onGestureHandler;
-	
-	public TmGesture(int _maxTouches, float _pinchTh, float _dblTapTime){
+
+	// _pinchTh = px
+	public TmGesture(int _maxTouches=4, float _pinchThPx=50f, float _dblTapTime=0.8f){
 		mMaxTouches = _maxTouches;
-		mPinchTh = Mathf.Abs(_pinchTh);
+		mPinchTh = Mathf.Abs(_pinchThPx);
 		mDblTapTime = _dblTapTime;
 		mSttPosition = new Vector2[_maxTouches];
 		mPosition = new Vector2[_maxTouches];
+		mHoldTime = new float[_maxTouches];
 		mActive = new bool[_maxTouches];
 		mGesture = new TmTouchEvent.Gesture[_maxTouches];
 		for(int i = 0; i < _maxTouches; ++i){
 			mActive[i] = false;
 			mGesture[i] = TmTouchEvent.Gesture.NONE;
+			mHoldTime[i]=0f;
 		}
 	}
 	
@@ -54,8 +59,15 @@ public class TmGesture{
 			if(fgId < mMaxTouches){
 				mActive[fgId] = true;
 				mPosition[fgId] = _tc[i].position;
+				mHoldTime[fgId] += Time.deltaTime;
 				if(_tc[i].phase == TouchPhase.Began){
 					mSttPosition[fgId] = _tc[i].position;
+					mHoldTime[fgId] = 0f;
+				}else if(_tc[i].phase == TouchPhase.Moved){
+					mHoldTime[fgId] = 0f;
+				}
+				if(_tc.Length!=1){
+					mHoldTime[fgId] = 0f;
 				}
 			}
 		}
@@ -70,6 +82,13 @@ public class TmGesture{
 			if((fId0<mActive.Length)&& mActive[fId0]){
 				Vector2 s0 = mSttPosition[fId0];
 				Vector2 p0 = mPosition[fId0];
+				if(mHoldTime[fId0] > mDblTapTime){
+					if((s0-p0).magnitude < mPinchTh*0.25f){
+						Touch[] tc = new Touch[1]{_tc[i]};
+						setEvent(tc,TmTouchEvent.Gesture.LONG_TAP);
+						continue;
+					}
+				}
 				for(int j = i+1; j < _tc.Length; ++j){
 					int fId1 =  _tc[j].fingerId;
 					if((fId1<mActive.Length)&& mActive[fId1]){
@@ -82,12 +101,12 @@ public class TmGesture{
 						Vector2 pc = p0+p1*0.5f;
 						bool isCenterFixed = ((pc-sc).magnitude<Mathf.Abs(chgDist)*0.5f);
 						if((chgDist > mPinchTh)&& isCenterFixed ){
-							int[] ids = new int[2]{Mathf.Min(fId0,fId1),Mathf.Max(fId0,fId1)};
-							setEvent(ids,TmTouchEvent.Gesture.PINCHI_OUT);
-						}
+							Touch[] tc = new Touch[2]{_tc[(fId0<fId1)?i:j],_tc[(fId0<fId1)?j:i]};
+							setEvent(tc,TmTouchEvent.Gesture.PINCHI_OUT);
+						}else
 						if((chgDist < -mPinchTh)&& isCenterFixed ){
-							int[] ids = new int[2]{Mathf.Min(fId0,fId1),Mathf.Max(fId0,fId1)};
-							setEvent(ids,TmTouchEvent.Gesture.PINCHI_IN);
+							Touch[] tc = new Touch[2]{_tc[(fId0<fId1)?i:j],_tc[(fId0<fId1)?j:i]};
+							setEvent(tc,TmTouchEvent.Gesture.PINCHI_IN);
 						}
 					}
 				}
@@ -98,15 +117,16 @@ public class TmGesture{
 #endif
 	}
 	
-	private bool setEvent(int[] _ids, TmTouchEvent.Gesture _gesture){
+	private bool setEvent(Touch[] _tc, TmTouchEvent.Gesture _gesture){
 		bool result = false;
-		if((onGestureHandler!=null)&&(_ids.Length>0)){
-			if(mGesture[_ids[0]]!=_gesture){
-				foreach(int id in _ids){
-					mGesture[id]=_gesture;
+		if((onGestureHandler!=null)&&(_tc.Length>0)){
+			if(mGesture[_tc[0].fingerId]!=_gesture){
+				foreach(Touch t in _tc){
+					mGesture[t.fingerId]=_gesture;
 				}
 				TmTouchEvent eve = new TmTouchEvent();
 				eve.gesture = _gesture;
+				eve.touches = _tc;
 				onGestureHandler(eve);
 				result = true;
 			}
