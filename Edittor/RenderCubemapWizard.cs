1b851a5ef8c70dfb480c7025cb7df6829efe21ce
misawa2048 <misawa@elix.jp>
2016-04-11 01:57:11 +0900

    RenderCubemapWizard: saveToPNG added.

--- Edittor/RenderCubemapWizard.cs
+++ Edittor/RenderCubemapWizard.cs
@@ -1,7 +1,9 @@
 ﻿// http://docs.unity3d.com/ja/current/ScriptReference/Camera.RenderToCubemap.html
+// http://stackoverflow.com/questions/34458622/unity-save-cubemap-to-one-circle-image
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.IO;
 
 public class RenderCubemapWizard : ScriptableWizard {
 
@@ -14,6 +16,9 @@ public class RenderCubemapWizard : ScriptableWizard {
 	}
 
 	void OnWizardCreate () {
+		if (renderFromPosition == null) {
+			renderFromPosition = Camera.main.transform;
+		}
 		// create temporary camera for rendering
 		GameObject go = new GameObject( "CubemapCamera");
 		go.AddComponent<Camera>();
@@ -22,7 +27,8 @@ public class RenderCubemapWizard : ScriptableWizard {
 		go.transform.rotation = Quaternion.identity;
 		// render into cubemap		
 		go.GetComponent<Camera>().RenderToCubemap( cubemap );
-
+		cubemap.Apply ();
+		ConvertToPng ();
 		// destroy temporary camera
 		DestroyImmediate( go );
 	}
@@ -31,5 +37,38 @@ public class RenderCubemapWizard : ScriptableWizard {
 	static void RenderCubemap () {
 		ScriptableWizard.DisplayWizard<RenderCubemapWizard>(
 			"Render cubemap", "Render!");
+	}
+
+	void ConvertToPng()
+	{
+		Debug.Log(Application.dataPath + "/" +cubemap.name +"_PositiveX.png");
+		var tex = new Texture2D (cubemap.width, cubemap.height, TextureFormat.RGB24, false);
+		// Read screen contents into the texture        
+		tex.SetPixels(cubemap.GetPixels(CubemapFace.PositiveX));        
+		// Encode texture into PNG
+		var bytes = tex.EncodeToPNG();      
+		File.WriteAllBytes(Application.dataPath + "/"  + cubemap.name +"_PositiveX.png", bytes);       
+
+		tex.SetPixels(cubemap.GetPixels(CubemapFace.NegativeX));
+		bytes = tex.EncodeToPNG();     
+		File.WriteAllBytes(Application.dataPath + "/"  + cubemap.name +"_NegativeX.png", bytes);       
+
+		tex.SetPixels(cubemap.GetPixels(CubemapFace.PositiveY));
+		bytes = tex.EncodeToPNG();     
+		File.WriteAllBytes(Application.dataPath + "/"  + cubemap.name +"_PositiveY.png", bytes);       
+
+		tex.SetPixels(cubemap.GetPixels(CubemapFace.NegativeY));
+		bytes = tex.EncodeToPNG();     
+		File.WriteAllBytes(Application.dataPath + "/"  + cubemap.name +"_NegativeY.png", bytes);       
+
+		tex.SetPixels(cubemap.GetPixels(CubemapFace.PositiveZ));
+		bytes = tex.EncodeToPNG();     
+		File.WriteAllBytes(Application.dataPath + "/"  + cubemap.name +"_PositiveZ.png", bytes);       
+
+		tex.SetPixels(cubemap.GetPixels(CubemapFace.NegativeZ));
+		bytes = tex.EncodeToPNG();     
+		File.WriteAllBytes(Application.dataPath + "/"  + cubemap.name   +"_NegativeZ.png", bytes);       
+		DestroyImmediate(tex);
+
 	}
 }
