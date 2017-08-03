8849e41b24c5deb898489f56afc7f0707f646ff5
Tsuyoshi Misawa mba2016 <tsuyoshimisawamba2016@TsuyoshinoMacBook-Air.local>
2017-08-03 11:40:20 +0900

    scripts:expDateTime added

---
+++ scripts/TmExpDayTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+namespace TmLib{
+	public class TmExpDayTime : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			DateTime expDt = new DateTime (2017, 10, 01, 0, 0, 0);
+			TimeSpan ts = expDt - DateTime.Now;
+			if (ts.Days < 0) {
+				Debug.Log ("the expiration date was reached");
+				Application.Quit ();
+			}
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		}
+	}
+}
