6faf671e11825dae71e491dfd70bbe43a4a39411
misawa2048 <misawa@elix.jp>
2017-11-29 21:28:45 +0900

    script : SingletonMonoBehaviour

---
+++ scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,32 @@
+﻿#define TmLib_SingletonMonoBehaviour_DontDestroyOnLoad
+using UnityEngine;
+
+// usage: public class xx : TmLib.SingletonMonoBehaviour<xx> {}
+namespace TmLib{
+	public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+		public const string NAME = "_singleton";
+		//---------------------------------------------------------
+		private static T m_Instance = null;
+		public static bool hasInstance{ get { return m_Instance!=null; } }
+		public static T Instance{
+			get{
+				if(m_Instance==null){
+					GameObject sysObj = new GameObject(NAME);
+					m_Instance = sysObj.AddComponent<T>();
+				}
+				return m_Instance;
+			}
+		}
+
+		protected void Awake () {
+			if(m_Instance==null){
+				m_Instance = FindObjectOfType<T>();
+				#if TmLib_SingletonMonoBehaviour_DontDestroyOnLoad
+				DontDestroyOnLoad(gameObject);
+				#endif
+			}else{
+				Destroy(this.gameObject);
+			}
+		}
+	}
+} //namespace TmLib
