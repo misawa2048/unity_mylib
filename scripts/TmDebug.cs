50ed683bb14c79fbeef009c15536b1430dcab42a
misawa2048 <misawa@elix.jp>
2018-12-23 13:18:45 +0900

    TmDebug : use [System.Diagnostics.Conditional("UNITY_EDITOR")] attribute

--- scripts/TmDebug.cs
+++ scripts/TmDebug.cs
@@ -1,56 +1,50 @@
-﻿#define TM_IS_DEBUG
+﻿// If you want to use other than 'UNITY_EDITOR',define that word to
+// "Project Settings > Player > Other Settings > Scripting Debug Symbol" 
 using UnityEngine;
 
 namespace TmLib{
 	public class TmDebug {
 
-		//---------------------------------------------------------------------------
-		public static void DrawLine(Vector3 start, Vector3 end){
-			#if TM_IS_DEBUG
+        //---------------------------------------------------------------------------
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void DrawLine(Vector3 start, Vector3 end){
 			DrawLine(start,end, Color.white,0.0f,true);
-			#endif
 		}
-		public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration=0f, bool depthTest=true){
-			#if TM_IS_DEBUG
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration=0f, bool depthTest=true){
 			Debug.DrawLine(start,end,color,duration,depthTest);
-			#endif
 		}
 
-		//---------------------------------------------------------------------------
-		public static void DrawRay(Vector3 start, Vector3 dir){
-			#if TM_IS_DEBUG
+        //---------------------------------------------------------------------------
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void DrawRay(Vector3 start, Vector3 dir){
 			DrawRay(start,dir,Color.white,0.0f,true);
-			#endif
 		}
-		public static void DrawRay(Vector3 start, Vector3 dir, Color color, float duration=0f, bool depthTest=true){
-			#if TM_IS_DEBUG
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void DrawRay(Vector3 start, Vector3 dir, Color color, float duration=0f, bool depthTest=true){
 			Debug.DrawRay(start,dir,color,duration,depthTest);
-			#endif
 		}
 
-		//---------------------------------------------------------------------------
-		public static void Log(object message){
-			#if TM_IS_DEBUG
+        //---------------------------------------------------------------------------
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void Log(object message){
 			Debug.Log(message);
-			#endif
 		}
-		public static void Log(object message,Object context){
-			#if TM_IS_DEBUG
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void Log(object message,Object context){
 			Debug.Log(message,context);
-			#endif
 		}
 
-		//---------------------------------------------------------------------------
-		public static void DrawMouseRay(float dist=1000f,Camera cam=null){
-			#if TM_IS_DEBUG
+        //---------------------------------------------------------------------------
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void DrawMouseRay(float dist=1000f,Camera cam=null){
 			if(cam == null) cam = Camera.main;
 			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 			DrawRay(ray.origin,ray.direction*dist,Input.GetMouseButton(0) ? Color.yellow : Color.gray);
-			#endif
 		}
-		//---------------------------------------------------------------------------
-		public static void DrawCircle( Vector3 _pos, Quaternion _rot, float _rad, Color _col, int _div=16){
-			#if TM_IS_DEBUG
+        //---------------------------------------------------------------------------
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void DrawCircle( Vector3 _pos, Quaternion _rot, float _rad, Color _col, int _div=16){
 			Vector3 stt=_pos, end;
 			float dd = 360f/(float)_div;
 			for(int i=0; i<=_div; ++i){
@@ -59,16 +53,15 @@ namespace TmLib{
 				if(i>0){ Debug.DrawLine(stt,end,_col); }
 				stt=end;
 			}
-			#endif
 		}
         //---------------------------------------------------------------------------
-        public static Vector3 GetCenterOfsRotPos(Vector3 _center, Vector3 _ofs, Quaternion _rot)
+        private static Vector3 GetCenterOfsRotPos(Vector3 _center, Vector3 _ofs, Quaternion _rot)
         {
             return _center + _rot * _ofs;
         }
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
         public static void DrawRect(Vector3 _center, Vector2 _rect, Quaternion _rot, Color _col)
         {
-#if TM_IS_DEBUG
             Vector3 p1 = GetCenterOfsRotPos(_center, new Vector3(-_rect.x, 0f, -_rect.y), _rot);
             Vector3 p2 = GetCenterOfsRotPos(_center, new Vector3(-_rect.x, 0f, _rect.y), _rot);
             Vector3 p3 = GetCenterOfsRotPos(_center, new Vector3(_rect.x, 0f, _rect.y), _rot);
@@ -77,7 +70,18 @@ namespace TmLib{
             Debug.DrawLine(p2, p3, _col);
             Debug.DrawLine(p3, p4, _col);
             Debug.DrawLine(p4, p1, _col);
-#endif
+        }
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void DrawCircleScale(Vector3 _center, int _divNum, float _sttOfs, float _endOfs, Quaternion _rot, Color _col)
+        {
+            for(int i=0; i < _divNum; ++i)
+            {
+                float ang = (float)i / (float)_divNum * 360f;
+                Quaternion ofsRot = Quaternion.AngleAxis(ang, Vector3.up);
+                Vector3 p1 = GetCenterOfsRotPos(_center, new Vector3(0f, 0f, _sttOfs), _rot* ofsRot);
+                Vector3 p2 = GetCenterOfsRotPos(_center, new Vector3(0f, 0f, _endOfs), _rot* ofsRot);
+                Debug.DrawLine(p1, p2, _col);
+            }
         }
     }
 } //namespace TmLib
